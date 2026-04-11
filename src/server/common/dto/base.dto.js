77776e9aab309849b395e54d15f6c292df06bb47
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2026-04-11 12:16:19 +0530

    Intial register service set-up.

---
+++ src/server/common/dto/base.dto.js
@@ -0,0 +1,3 @@
+class BaseDTO {}
+
+export default BaseDTO;
---
+++ src/server/module/auth/register/register.dto.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+import BaseDTO from "../../../common/dto/base.dto.js";
+
+class RegisterDTO extends BaseDTO {
+  static schema = Joi.object({
+    username: Joi.string().trim().min(4).max(27).required(),
+    email: Joi.string().email().trim().max(322).required(),
+    password: Joi.string()
+      .trim()
+      .min(6)
+      .message("Password should be minium 6 characters long.")
+      .max(14)
+      .required(),
+  });
+}
+
+export default RegisterDTO;

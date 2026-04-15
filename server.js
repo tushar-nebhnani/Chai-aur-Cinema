fe9a1e0d5d1311c722332ed3c31f421feb688cc3
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2026-04-15 18:44:35 +0530

    Added proper path to app.js

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ import "dotenv/config";
 import app from "./src/server/app.js";
 import initializeDatabase from "./src/server/common/db/init-db.js";
 
+const PORT = process.env.PORT || 3000;
+
 try {
-  if (process.env.NODE_ENV !== "production") {
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, async () => {
-      await initializeDatabase();
-      console.log(`Server is running at port ${PORT} ...`);
-    });
-  }
+  app.listen(PORT, async () => {
+    await initializeDatabase();
+    console.log(`Server is running at port ${PORT} ...`);
+  });
 } catch (error) {
   console.error("Error while starting the server", error);
 }

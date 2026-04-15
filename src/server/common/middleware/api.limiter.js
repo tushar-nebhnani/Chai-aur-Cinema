821b53632a4b40e05a08816ce8bc96a6c493e86b
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2026-04-15 22:00:31 +0530

    Increased the API call limit.

--- src/server/common/middleware/api.limiter.js
+++ src/server/common/middleware/api.limiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 20,
+  max: 30,
   message: {
     success: false,
     message:
